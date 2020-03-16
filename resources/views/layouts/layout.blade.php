d8a9c25dc92f8e68a7f351eb16683e44cff9c7da
Christian <chrgiga@gmail.com>
2020-03-16 13:29:12 +0100

    feat: add nav item to layout template

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,7 +9,35 @@
 </head>
 <body>
 
-<div class="container-fluid" style="margin-top: 100px">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <nav class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Department CRUD</a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="{{ (request()->is('users*')) ? 'active' : '' }}">
+                                <a href="{{ route('users.index') }}">Users</a>
+                            </li>
+                            <li class="{{ (request()->is('departments*')) ? 'active' : '' }}">
+                                <a href="{{ route('departments.index') }}">Departments</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
     @yield('content')
 </div>
 <style type="text/css">
