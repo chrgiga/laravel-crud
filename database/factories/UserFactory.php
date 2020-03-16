5f423c74f34a8e64ff6dd746ddfb81348072b91f
Christian <chrgiga@gmail.com>
2020-03-16 14:05:20 +0100

    feat(seeder): populate user table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,18 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $works = [
+        ['position' => 'PHP developer', 'functions' => 'Support on IT projects'],
+        ['position' => 'Technical assistant', 'functions' => 'Customer technical support'],
+        ['position' => 'Receptionist', 'functions' => 'Manage customer visits'],
+    ];
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'work_position' => $works[array_rand($works)]['position'],
+        'work_functions' => $works[array_rand($works)]['functions'],
     ];
 });
